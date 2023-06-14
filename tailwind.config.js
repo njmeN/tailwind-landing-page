a35be0400eb247bf908ad4ab1f08a155ed03721b
najmenoor490@gmail.com <njmeN>
2023-06-14 23:31:40 +0330

    #

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"976px",
+      xl:"1440px",
+    },
+    extend: {
+      colors:{
+        /* Primary*/
+      BrightRed: "hsl(12, 88%, 59%)",
+      BrightRedLight:"hsl(12, 88%, 87%)",
+      DarkBlue:" hsl(228, 39%, 23%)",
+        /*Neutral*/
+      DarkGrayishBlue: "hsl(227, 12%, 61%)",
+      VeryDarkBlue:"hsl(233, 12%, 13%)",
+      VeryPaleRed: "hsl(13, 100%, 96%)",
+      VaryLightGray: "hsl(0, 0%, 98%)",
+
+      }
+    },
+  },
+  plugins: [],
+}
+
